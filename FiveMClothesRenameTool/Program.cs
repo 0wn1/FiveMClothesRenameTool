59204825ff014aec24a243eed732a92a1b2969b5
0wn1 <thebigboy@outlook.com.br>
2023-03-08 15:38:28 -0300

    Initial commit

---
+++ FiveMClothesRenameTool/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+
+namespace FiveMClothesRenameTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FiveM Clothes Rename Tool!");
+
+            string sourceFolderPath = string.Empty;
+            while (string.IsNullOrEmpty(sourceFolderPath))
+            {
+                Console.Write("Please enter the path of the source folder: ");
+                sourceFolderPath = Console.ReadLine();
+            }
+
+            string backupOption = string.Empty;
+            bool backup = false;
+
+            while (string.IsNullOrEmpty(backupOption))
+            {
+                Console.Write("Do you want to create a backup of the source folder? (Y/N): ");
+                backupOption = Console.ReadLine().Trim().ToUpper();
+                if (backupOption == "Y")
+                {
+                    backup = true;
+                }
+                else if (backupOption == "N")
+                {
+                    backup = false;
+                }
+                else
+                {
+                    backupOption = string.Empty;
+                }
+            }
+
+            string backupFolderPath = string.Empty;
+            if (backup)
+            {
+                backupFolderPath = sourceFolderPath + "_new";
+                try
+                {
+                    Directory.CreateDirectory(backupFolderPath);
+                    string[] extensions = { ".ydd", ".ytd" };
+                    string[] filePaths = Directory.GetFiles(sourceFolderPath, "*.*", SearchOption.AllDirectories);
+                    int count = 0;
+                    foreach (string filePath in filePaths)
+                    {
+                        string extension = Path.GetExtension(filePath);
+                        if (Array.IndexOf(extensions, extension) >= 0)
+                        {
+                            string subFolder = Path.GetDirectoryName(filePath).Replace(sourceFolderPath, "").TrimStart('\\');
+                            string fileName = Path.GetFileName(filePath);
+                            string newSubFolderPath = Path.Combine(backupFolderPath, subFolder);
+                            Directory.CreateDirectory(newSubFolderPath);
+                            string newFilePath = Path.Combine(newSubFolderPath, fileName);
+                            File.Copy(filePath, newFilePath, true);
+                            count++;
+                        }
+                    }
+                    Console.WriteLine("Backup created successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to create backup: " + ex.Message);
+                    Console.ReadLine();
+                    return;
+                }
+                sourceFolderPath = backupFolderPath;
+            }
+
+            Console.Write("Please enter the prefix to be added to the file names (press enter to use subfolder name as prefix): ");
+            string prefix = Console.ReadLine().Trim();
+            if (string.IsNullOrEmpty(prefix))
+            {
+                prefix = "{0}^";
+            }
+            else if (!prefix.EndsWith("^"))
+            {
+                prefix += "^";
+            }
+
+            string[] extensionsToRename = { ".ydd", ".ytd" };
+            string[] filePathsToRename = Directory.GetFiles(sourceFolderPath, "*.*", SearchOption.AllDirectories);
+            int renamedFileCount = 0;
+            foreach (string filePath in filePathsToRename)
+            {
+                string extension = Path.GetExtension(filePath);
+                if (Array.IndexOf(extensionsToRename, extension) >= 0)
+                {
+                    string subFolder = Path.GetDirectoryName(filePath).Replace(sourceFolderPath, "").TrimStart('\\');
+                    string fileName = string.Format(prefix, subFolder) + Path.GetFileName(filePath);
+                    string newFilePath = Path.Combine(Path.GetDirectoryName(filePath), fileName);
+                    try
+                    {
+                        File.Move(filePath, newFilePath);
+                        Console.WriteLine("Renamed file: {0} -> {1}", filePath, newFilePath);
+                        renamedFileCount++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Failed to rename file: {0} -> {1}. {2}", filePath, newFilePath, ex.Message);
+                    }
+                }
+            }
+
+            Console.WriteLine("Renamed {0} files.", renamedFileCount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# FiveM Clothes Rename Tool
+This is a command-line tool written in C# that allows users to rename GTA V clothes files to FiveM clothing files.
+
+------------
+### Prerequisites
+.NET v4.7.2
+
+------------
+### How it Works
+The tool prompts the user to enter the path to the source folder containing the clothes files to be renamed. The user is then asked if they want to create a backup of the source folder. If the user chooses to create a backup, a new folder is created with the suffix "_new" appended to the original folder name. All the files in the source folder with the extensions ".ydd" and ".ytd" are copied to the backup folder.
+
+After creating the backup (if applicable), the user is prompted to enter a prefix to be added to the file names. If the user enters nothing and just presses enter, the subfolder name of each file will be used as the prefix. If the prefix entered by the user does not end with a caret "^" character, the tool will append it automatically.
+
+Finally, the tool searches for all the files with the extensions ".ydd" and ".ytd" in the source folder and its subfolders, and renames them according to the prefix entered by the user. The new file name is constructed by concatenating the prefix and the original file name. If a file with the new name already exists, the tool will overwrite it.
+
+------------
+### Usage
+To use the tool, simply compile the code into an executable file and run it from the command line. Make sure to pass any necessary arguments, such as the path to the source folder.
+
+Note that this tool was designed specifically for use with FiveM clothes files and may not work properly with other types of files. Also, be sure to use it responsibly and make backups of your files before using it.
+
+------------
+### Preview
+Streamable: [View](https://streamable.com/aoykcv "View")
